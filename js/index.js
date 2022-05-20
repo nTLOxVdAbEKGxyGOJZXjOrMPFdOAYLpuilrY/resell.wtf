1ab09a0b096a4ee2ba47058903b4048bf5633749
nTLOxVdAbEKGxyGOJZXjOrMPFdOAYLpuilrY <97568527+nTLOxVdAbEKGxyGOJZXjOrMPFdOAYLpuilrY@users.noreply.github.com>
2022-05-20 15:44:35 -0700

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var option = {
+    strings: ['<i>craze#0001 - 811216525322616902</i>', '<i>act broke to stay rich</i>', '<i>Knucks - Home</i>'],
+    typeSpeed: 55,
+    backSpeed: 65,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false
+}
+
+var typed = new Typed(".typed", option)
+
+var option2 = {
+    strings: ['<i>click to enter</i>'],
+    typeSpeed: 55,
+    loop: false,
+    showCursor: true
+}
+
+var typed2 = new Typed("#entertext", option2)
+
+function enterSite() {
+    document.getElementById("song").play();
+    document.getElementById("enter").className = "animate__animated animate__fadeOut";
+    document.getElementById('entertext').remove();
+    setTimeout(() => {
+            document.getElementById("enter").remove();
+    }, 2000)
+}
